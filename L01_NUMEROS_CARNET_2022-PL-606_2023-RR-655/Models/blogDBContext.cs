fa77b83c0de636d224df5b2778870ffc670a4119
HenryPresidente <Henry@codersfree.com>
2025-02-20 18:38:23 -0600

    Conexion con la BD

---
+++ L01_NUMEROS_CARNET_2022-PL-606_2023-RR-655/Models/blogDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace L01_NUMEROS_CARNET_2022_PL_606_2023_RR_655.Models
+{
+    public class blogDBContext: DbContext
+    {
+        public blogDBContext(DbContextOptions<blogDBContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- L01_NUMEROS_CARNET_2022-PL-606_2023-RR-655/Program.cs
+++ L01_NUMEROS_CARNET_2022-PL-606_2023-RR-655/Program.cs
@@ -1,8 +1,17 @@
+using L01_NUMEROS_CARNET_2022_PL_606_2023_RR_655.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<blogDBContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("blogDBConnection")
+        )
+     );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
